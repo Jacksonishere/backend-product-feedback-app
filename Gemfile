088e58b0082e2e8ae06418b3694f21bd1e3be7bc
Jackson Lu <lujackson355@gmail.com>
2022-09-10 21:16:41 -0400

    added feedbacks controller with index method that supports pagination

--- Gemfile
+++ Gemfile
@@ -44,3 +44,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'jsonapi-serializer'
 gem 'faker'
+gem 'kaminari'
--- app/controllers/api/v1/feedbacks_controller.rb
+++ app/controllers/api/v1/feedbacks_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::FeedbacksController < ApplicationController
   def index
-    feedbacks = Feedback.all
-    render json: FeedbackSerializer.new(feedbacks, include: [:user]).serializable_hash.to_json
+    feedbacks = Feedback.page(params[:page_num] || 1).per(15)
+    render json: { feedbacks: FeedbackSerializer.new(feedbacks, include: [:user]).serializable_hash }.to_json
   end
 
   def show
@@ -14,5 +14,13 @@ class Api::V1::FeedbacksController < ApplicationController
 
   def update
 
+  end
+  
+  def create
+    
+  end
+
+  def feedback_params
+    # params.permit(:page_num)
   end
 end
