3c1b3e072dc7fa84ba034c703a67088a1a30d92e
Jackson Lu <lujackson355@gmail.com>
2022-09-19 09:32:55 -0400

    Added sortable concern for sorting feedbacks

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,6 @@
 class Feedback < ApplicationRecord
   include Filterable
+  include Sortable
   
   default_scope { order(created_at: :desc) } 
   
@@ -9,4 +10,10 @@ class Feedback < ApplicationRecord
   has_many :likes, as: :likeable
 
   scope :filter_by_category, -> (category) { where('category = ?', category) }
+  
+  scope :sort_by_likes, -> (order) { 
+    left_joins(:likes). # left join to generate rows with feedback and like cols
+    group(:id).      # group the rows by feedback id, so we can run aggregate functions
+    order("count(likes.id) #{order}")
+   }
 end
